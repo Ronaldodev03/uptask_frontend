7bff944c7e38e68d7784e76e1d46e1cb0360aa6e
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2024-12-28 01:47:50 -0400

    add AuthLayout & login & register (routes, api functions, ZOD schema & views)

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,9 @@ import DashboardView from "@/views/DashboarView";
 import CreateProjectView from "./views/projects/CreateProjectView";
 import EditProjectView from "./views/projects/EditProjectView";
 import ProjectDetailsView from "./views/projects/ProjectDetailsView";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginView from "./views/auth/LoginView";
+import RegisterView from "./views/auth/RegisterView";
 
 export default function Router() {
   return (
@@ -17,6 +20,11 @@ export default function Router() {
             path="/projects/:projectId/edit"
             element={<EditProjectView />}
           />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/login" element={<LoginView />} />
+          <Route path="/auth/register" element={<RegisterView />} />
         </Route>
       </Routes>
     </BrowserRouter>
